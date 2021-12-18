70ff18a6e6aa3ec54bd7ba7f0194f6719a571c85
kelu22 <raquel.noblejas.sampedro@gmail.com>
2021-12-18 10:46:58 -0500

    Namespace changed

--- Test2MVC/App_Start/BundleConfig.cs
+++ Test2MVC/App_Start/BundleConfig.cs
@@ -1,7 +1,7 @@
 ﻿using System.Web;
 using System.Web.Optimization;
 
-namespace Test2MVC
+namespace Photography_MVC
 {
     public class BundleConfig
     {
--- Test2MVC/Properties/AssemblyInfo.cs
+++ Test2MVC/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Test2MVC")]
+[assembly: AssemblyTitle("Photography_MVC")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Netcracker Tech")]
-[assembly: AssemblyProduct("Test2MVC")]
+[assembly: AssemblyProduct("Photography_MVC")]
 [assembly: AssemblyCopyright("Copyright © Netcracker Tech 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
