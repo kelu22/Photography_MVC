5e6a7e2570012dc5c0dbdbe6fbeedf386c5235df
kelu22 <raquel.noblejas.sampedro@gmail.com>
2022-01-04 06:15:48 -0500

    Class 12/15 added. the 3 Layers are created and connnection with DB in Data Access Layer has been coded.

---
+++ DataAccess/DL_Class.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DataAccess
+{
+    public class DL_Class
+    {
+        public static DataSet GetContactUsingDBWithConfig()
+        {
+            DataTable dt = new DataTable();
+            DataSet dsContacts = new DataSet();
+
+            //Config Details and SQL Connection
+            SqlConnection con;
+            var cs = ConfigurationManager.ConnectionStrings["ContactsConnection_DEV"].ConnectionString;
+
+            con = new SqlConnection(cs);
+            string sql = null;
+            sql = "Select * from Contacts";
+
+            //Open SQL Connection
+            con.Open();
+
+            SqlCommand command;
+            command = new SqlCommand(sql, con);
+
+            SqlDataReader dataReader;
+            dataReader = command.ExecuteReader();
+
+            dt.Load(dataReader);
+            dsContacts.Tables.Add(dt);
+
+            dataReader.Close();
+            command.Dispose();
+            con.Close();
+
+            return dsContacts;
+        }
+    }
+}
+}
---
+++ Models/ML_Class.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Models
+{
+    public class ML_Class
+    {
+        //Contact information properties
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [DisplayName("Email")]
+        public string Email { get; set; }
+
+
+        //Any properties related to the photographies
+    }
+}
