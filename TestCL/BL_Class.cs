910e7b0f043a5013ccbd99fe671bb27158c67bf9
kelu22 <raquel.noblejas.sampedro@gmail.com>
2022-01-09 15:28:05 -0500

    Business Layer added

--- TestCL/BL_Class.cs
+++ TestCL/BL_Class.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using Models;
+using DataAccess;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,26 @@ namespace TestCL
 {
     public class BL_Class
     {
+        public static List<ML_Class> GetContactData()
+        {
+            ML_Class contact = new ML_Class();
+            var contactList = new List<ML_Class>();
+
+            DataSet dsContacts = new DataSet();
+            dsContacts = DL_Class.GetContactUsingDBWithConfig();
+
+            //Transfer DataSet to Object
+            if (dsContacts.Tables.Count > 0)
+            {
+                contactList = dsContacts.Tables[0].AsEnumerable().Select(m => new ML_Class()
+                {
+                    FirstName = Convert.ToString(m["FirstName"]),
+                    LastName = Convert.ToString(m["LastName"]),
+                    Email = Convert.ToString(m["Email"])
+                }).ToList();
+            }
+
+            return contactList;
+        }
     }
 }
